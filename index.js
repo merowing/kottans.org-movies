d3ac963e9d6fa8dbf81f5e5b3b1f0cdfdbb889dd
elgynart <elgynart@gmail.com>
2022-03-22 11:00:51 +0200

    create separate files style.css&index.js from index.html

---
+++ index.js
@@ -0,0 +1,118 @@
+let data = [
+    {
+        image: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSyXO9PGwU8Y2lv7JONiXLR7jHW4e8PyiCE6lTIVzWrZ8NvKpms",
+        name: "Harry Potter and the Goblet of Fire",
+        info: {
+            director: "Mike Newell",
+            actors: ["Daniel Radcliffe", "Rupert Grint", "Emma Watson"],
+            budget: "$150 million",
+            release: "6 November 2005"
+        },
+        description: `<p><strong>Harry Potter and the Goblet of Fire</strong> is a 2005 fantasy film directed by Mike Newell and distributed by Warner Bros. Pictures, based on J. K. Rowling's 2000 novel of the same name. Produced by David Heyman and written by Steve Kloves, it is the sequel to Harry Potter and the Prisoner of Azkaban (2004) and the fourth instalment in the Harry Potter film series. The film stars Daniel Radcliffe as Harry Potter, with Rupert Grint as Ron Weasley, and Emma Watson as Hermione Granger. Its story follows Harry's fourth year at Hogwarts as he is chosen by the Goblet of Fire to compete in the Triwizard Tournament.</p>
+                <p>The film is the first of the series to receive a PG-13 certificate in the US, and a 12A in the UK. Filming began in early 2004. The Hogwarts scenes were shot at the Leavesden Film Studios. Five days after its release, the film had grossed over US$102 million at the North American box office, which is the third-highest first-weekend tally for a Harry Potter film behind Harry Potter and the Deathly Hallows – Part 1 and Part 2. Goblet of Fire enjoyed an immensely successful run at the box office, earning $896 million worldwide, which made it the highest-grossing film of 2005 and the sixth-highest-grossing film in the Harry Potter series.</p>
+                <p>The film was nominated for the Academy Award for Best Art Direction, and won the BAFTA Award for Best Production Design. Goblet of Fire was the second film in the series to be released in IMAX. The film is one of the best-reviewed instalments within the series, being praised for the higher level of maturity and sophistication of its characters, plotline, tone, screenplay, and the performances of the lead actors. It was followed by Harry Potter and the Order of the Phoenix in 2007.</p>`
+    },
+    {
+        image: "https://pbs.twimg.com/media/Ejv36UaU8AAlK9B.jpg",
+        name: "Alita: Battle Angel",
+        info: {
+            director: "Robert Rodriguez",
+            actors: ["Rosa Salazar", "Christoph Waltz", "Jennifer Connelly", "Mahershala Ali"],
+            budget: "$150 - 200 million",
+            release: "14 February 2019"
+        },
+        description: `<p><strong>Alita: Battle Angel</strong> is a 2019 American cyberpunk action film based on Japanese manga artist Yukito Kishiro's 1990s series Battle Angel Alita and its 1993 original video animation adaptation, Battle Angel. It was directed by Robert Rodriguez, produced by James Cameron and written by Cameron and Laeta Kalogridis. Rosa Salazar stars through performance-capture animation as Alita, a cyborg who awakens in a new body with no memory of her past and sets out to uncover her destiny. Christoph Waltz, Jennifer Connelly, Mahershala Ali, Ed Skrein, Jackie Earle Haley and Keean Johnson star in supporting roles.</p>
+                <p>Announced in 2003, production was repeatedly delayed due to Cameron's work on Avatar (2009) and its sequels. After years of development hell, Rodriguez was announced as Alita's director in April 2016, with Salazar cast as the lead the following month. Principal photography began in October 2016 in Austin, Texas, mostly at Rodriguez's Troublemaker Studios, and lasted until February 2017.</p>
+                <p>Alita: Battle Angel had its world premiere at the Odeon Leicester Square on January 31, 2019, and was released in Hong Kong, Indonesia, Macau, Malaysia, Singapore, South Korea, and Taiwan on February 5, 2019, marking Chinese New Year. It was released in the United States on February 14, 2019, by 20th Century Fox in RealD 3D, Dolby Cinema, 4DX, ScreenX, and IMAX 3D formats. It is the first film produced by Lightstorm Entertainment since Avatar, and the last film released by 20th Century Fox before the acquisition of 21st Century Fox by Disney. The film grossed over $404 million worldwide, making it Rodriguez's highest-grossing film, but with a reported break-even point of $350–500 million, there is debate over whether it was profitable. It received mixed-to-positive reviews from critics, with praise for Salazar's performance, the action scenes and the visual effects but criticism for the screenplay.</p>
+                <p>The film was nominated for the Academy Award for Best Art Direction, and won the BAFTA Award for Best Production Design. Goblet of Fire was the second film in the series to be released in IMAX. The film is one of the best-reviewed instalments within the series, being praised for the higher level of maturity and sophistication of its characters, plotline, tone, screenplay, and the performances of the lead actors. It was followed by Harry Potter and the Order of the Phoenix in 2007.</p>`
+    },
+    {
+        image: "https://static1.colliderimages.com/wordpress/wp-content/uploads/2020/07/tenet-august-poster-john-david-washington-double-405x600.jpg?q=50&fit=crop&w=405&dpr=1.5",
+        name: "TENET",
+        info: {
+            director: "Christopher Nolan",
+            actors: ["John David Washington", "Robert Pattinson", "Elizabeth Debicki"],
+            budget: "$200 million",
+            release: "3 September 2020"
+        },
+        description: `<p><strong>Tenet</strong> is a 2020 science fiction action-thriller film written and directed by Christopher Nolan, who produced it with Emma Thomas. A co-production between the United Kingdom and United States, it stars John David Washington, Robert Pattinson, Elizabeth Debicki, Dimple Kapadia, Michael Caine, and Kenneth Branagh. The plot follows a secret agent (Washington) as he manipulates the flow of time to prevent World War III.</p>
+                <p>Nolan took more than five years to write the screenplay after deliberating about Tenet's central ideas for over a decade. Pre-production began in late 2018, casting in March 2019, and principal photography lasted three months in Denmark, Estonia, India, Italy, Norway, the United Kingdom, and United States, from May to November. Cinematographer Hoyte van Hoytema shot on 70 mm and IMAX. Scenes of time manipulation were filmed both backwards and forwards. Upwards of one hundred vessels and thousands of extras were used.</p>
+                <p>Delayed three times because of the COVID-19 pandemic, Tenet was released in the United Kingdom on August 26, 2020, and United States on September 3, 2020, in IMAX, 35 mm, and 70 mm. It was the first Hollywood tent-pole to open in theaters after the pandemic shutdown, and grossed $362 million worldwide, making it the fourth-highest-grossing film of 2020. However, it failed to break even due to its costly production and marketing budget, losing distributor Warner Bros. Pictures as much as $100 million. The film received generally favorable reviews from critics on Metacritic, with Rotten Tomatoes saying it had "all the cerebral spectacle audiences expect from a Christopher Nolan production."</p>`
+    },
+    {
+        image: "https://m.media-amazon.com/images/M/MV5BNDJiZDliZDAtMjc5Yy00MzVhLThkY2MtNDYwNTQ2ZTM5MDcxXkEyXkFqcGdeQXVyMDA4NzMyOA@@._V1_.jpg",
+        name: "The Old Guard",
+        info: {
+            director: "Gina Prince-Bythewood",
+            actors: ["Charlize Theron", "KiKi Layne", "Marwan Kenzari", "Luca Marinelli", "Harry Melling", "Veronica Ngo", "Matthias Schoenaerts", "Chiwetel Ejiofor"],
+            budget: "$70 million",
+            release: "10 July 2020"
+        },
+        description: `<p><strong>The Old Guard</strong> is a 2020 American superhero film directed by Gina Prince-Bythewood and written by Greg Rucka, based on his comic book of the same name. The film stars Charlize Theron, KiKi Layne, Matthias Schoenaerts, Marwan Kenzari, Luca Marinelli, Harry Melling, Van Veronica Ngo and Chiwetel Ejiofor, and follows a team of immortal mercenaries on a revenge mission.</p>
+                <p>The Old Guard was released on July 10, 2020, on Netflix. It received generally positive reviews from critics, with praise for the action sequences and Theron's performance.</p>`
+    }
+];
+
+let movieBlock = document.querySelector("#menu");
+let prevActiveMovieIndex = 0;
+movieBlock.addEventListener("click", e => {
+    let ind = [...movieBlock.querySelectorAll("li")].indexOf(e.target);
+
+    movieBlock.querySelectorAll("li")[ind].setAttribute("class", "active");
+    movieBlock.querySelectorAll("li")[prevActiveMovieIndex].removeAttribute("class");
+    prevActiveMovieIndex = ind;
+
+    if(document.body.offsetWidth <= 500) checkMobileMenu();
+
+    loadMovie(ind);
+});
+
+function loadMovie(movieId = 0) {
+    let movie = data[movieId];
+    let block = document.querySelector(".block");
+
+    if(movieId > data.length - 1 || !data.length) {
+        alert("data empty!");
+        return;
+    }
+    block.innerHTML = "";
+    let htmlBlock = `
+        <div class="mobile-title">${movie.name}</div>
+        <div class="top-info">
+            <div class="imageBlock">
+                <img src="${movie.image}" alt="${movie.name}" />
+            </div>
+            <div class="block-info">
+                <ul>
+                    <li><span>Director:</span><span>${movie.info.director}</span></li>
+                    <li><span>Actors:</span><span>${movie.info.actors.join(", ")} and others</span></li>
+                    <li><span>Budget:</span><span>${movie.info.budget}</span></li>
+                    <li><span>Release Date:</span><span>${movie.info.release}</span></li>
+                </ul>
+            </div>
+        </div>
+        <div class="description">${movie.description}</div>`;
+
+    block.insertAdjacentHTML('beforeend', htmlBlock);
+}
+loadMovie();
+
+// mobile menu block
+let mobileMenu = document.querySelector("#mobileButton-menu");
+let menu = document.querySelector("#menu");
+
+function checkMobileMenu() {
+    if(!menu.getAttribute("class")) {
+        menu.setAttribute("class", "mobile");
+        mobileMenu.innerText = "close";
+        document.body.style.overflow = "hidden";
+    }else {
+        menu.removeAttribute("class");
+        mobileMenu.innerText = "menu";
+        document.body.removeAttribute("style");
+    }
+
+    window.scrollTo(0, 0);
+}
+
+mobileMenu.addEventListener("click", () => checkMobileMenu());
